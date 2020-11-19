ab0283b92eefb5e8bea2bcbac92f857bf20bf628
Joe Himes <joehimes42@gmail.com>
2020-11-19 16:32:37 -0500

    Added User model/controller. Added signup/signin function for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'users#new'
+
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'signup' => 'users#new', as: 'signup'
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
